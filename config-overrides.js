6f9f442ff5399601e938a174d1986748588a40e2
terrykingcha <terrykingcha@gmail.com>
2021-05-10 18:41:58 +0800

    add gh-pages

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const { override, adjustStyleLoaders } = require('customize-cra');
-
-module.exports = {
-  webpack: override(
-    adjustStyleLoaders(({ use: [ , css] }) => {
-      css.options.url = false;
-    })
-  )
-}
